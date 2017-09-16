7e9d6f034c3c04e93490cdb69c61175de0d90047
Sukriti20 <31579547+Sukriti20@users.noreply.github.com>
2017-09-15 23:13:44 -0700

    Exercise_1 reading csv files

---
+++ Data_Analysis_Process/Reading_CSV.py
@@ -0,0 +1,36 @@
+import unicodecsv
+
+#enrollments_filename = 'enrollments.csv'
+
+## Longer version of code (replaced with shorter, equivalent version below)
+
+# enrollments = []
+# f = open(enrollments_filename, 'rb')
+# reader = unicodecsv.DictReader(f)
+# for row in reader:
+#     enrollments.append(row)
+# f.close()
+
+'''with open(enrollments_filename, 'rb') as f:
+    reader = unicodecsv.DictReader(f)
+    enrollments = list(reader)'''
+    
+
+engagement_filename = '/datasets/ud170/udacity-students/daily_engagement.csv'
+submissions_filename = '/datasets/ud170/udacity-students/project_submissions.csv'
+
+engagement_list = []
+with open(engagement_filename,'rb') as f:
+    reader = unicodecsv.DictReader(f)
+    engagement_list = list(reader)
+    
+with open(submissions_filename,'rb') as f:
+    reader = unicodecsv.DictReader(f)
+    submission_list= list(reader)
+    
+    
+daily_engagement = engagement_list    
+project_submissions = submission_list
+
+print(daily_engagement[0])
+print(project_submissions[0])
